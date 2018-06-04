2e1e1ef28c043a12cb28d514ceaa01872bdcec4d
GonzalezAdriana <gonzalez97glez@gmail.com>
2018-06-04 16:47:28 -0500

    congreso m y p

---
+++ Views/mcongreso/index.php
@@ -0,0 +1,91 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+
+<article>
+
+    <header>
+
+        <br>
+
+        <h2  align="lefth"> Congreso Medico </h2>
+
+    </header>
+
+    <main>
+        <form action="">
+            <div align ="rigth">
+
+
+                <div  class="row">
+                    <form class="col s12">
+
+                        <p>
+                        <div class="input-field col s6">
+                            <label>
+                                <input type="checkbox" />
+                                <span>Medicina Interna, del paciente critico coordinador</span>
+                            </label>
+                        </div>
+                        </p>
+                        <p>
+
+
+                        <p>
+                        <div class="input-field col s6">
+                            <label>
+                                <input type="checkbox" />
+                                <span>Pediatria coordinacion</span>
+
+                            </label>
+                        </div>
+                        </p>
+                        <p>
+
+                        <p>
+                        <div class="input-field col s6">
+                            <label>
+                                <input type="checkbox" />
+                                <span>Actualizacion en enfermedad cardiovascular </span>
+                            </label>
+                        </div>
+                        </p>
+                        <p>
+
+
+                        <p>
+                        <div class="input-field col s6">
+                            <label>
+                                <input type="checkbox" />
+                                <span>Cirujia coordinador</span>
+
+                            </label>
+                        </div>
+                        </p>
+                        <p>
+
+                        <p>
+                        <div class="input-field col s6">
+                            <label>
+                                <input type="checkbox" />
+                                <span>Gineco-obstetricia coordinador</span>
+
+                            </label>
+                        </div>
+                        </p>
+                        <p>
+                    </form>
+                </div>
+
+    </main>
+
+</article>
+
+</body>
+</html>
+
---
+++ Views/pcongreso/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+</head>
+<body>
+
+<article>
+
+    <header>
+
+        <br>
+
+        <h2  align="left">Pre-Congreso</h2>
+
+    </header>
+
+    <main>
+        <form action="">
+            <div align ="rigth">
+
+
+                <div  class="row">
+                    <form class="col s12">
+
+                        <p>
+                        <div class="input-field col s6">
+                            <label>
+                                <input type="checkbox" />
+                                <span>Insulinoterapia</span>
+
+                            </label>
+                        </div>
+                        </p>
+                        <p>
+
+
+                        <p>
+                        <div class="input-field col s6">
+                            <label>
+                                <input type="checkbox" />
+                                <span>Abordaje de Via Aerea Dificil</span>
+
+                            </label>
+                        </div>
+                        </p>
+                        <p>
+                    </form>
+                </div>
+
+    </main>
+
+</article>
+
+</body>
+</html>
+
+
+
+
+
